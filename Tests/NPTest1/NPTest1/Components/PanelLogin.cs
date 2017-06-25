32807ca639acb9f2cba330b472c7a6f8aecee555
andranick <andranick@bk.ru>
2017-06-25 19:24:35 +0300

    В проект добавлено ViewComponent

---
+++ Tests/NPTest1/NPTest1/Components/PanelLogin.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace NPTest1.Components
+{
+    public class PanelLogin:ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            if (HttpContext.User.Identity.IsAuthenticated)
+            {
+                // если идентфицирован выводим панель с названием учетной записи
+                return View("LoginNamePanelView","User");
+            }
+            else
+                // если нет, панель для регистрации
+                return View("LoginInputPanelView");
+        }
+    }
+}
---
+++ Tests/NPTest1/NPTest1/Controllers/LoginController.cs
@@ -0,0 +1,36 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace NPTest1.Controllers
+{
+    public class LoginController : Controller
+    {
+        public ActionResult LoginPanel()
+        {
+            return PartialView("LoginInputPanelView");
+            /*
+            if (HttpContext.User.Identity.IsAuthenticated)
+            {
+                FormsIdentity id = (FormsIdentity)User.Identity;
+                FormsAuthenticationTicket ticket = id.Ticket;
+                try
+                {
+                    UserViewModel us = DBAuthentication.GetLoginById(Convert.ToInt32(ticket.UserData));
+                    if (us == null || us.LoginViewName == null)
+                        throw new Exception();
+                    return PartialView("LoginNamePanelView", us.LoginViewName);
+                }
+                catch
+                {
+                    return PartialView("LoginNamePanelView", HttpContext.User.Identity.Name);
+                }
+            }
+            else
+                return PartialView("LoginInputPanelView");
+            */
+        }
+    }
+}
