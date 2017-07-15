302f63d61dc9f46e521d5eb66813c957d54b5179
andranick <andranick@bk.ru>
2017-07-15 16:20:56 +0300

    submit через ajax универсальным обрабочиком для всех форм

--- NotePlot/wwwroot/js/noteplot.js
+++ NotePlot/wwwroot/js/noteplot.js
@@ -44,3 +44,37 @@ function OpenDialog(hr) {
             $(this).dialog("open"); // Открываем диалог только после загрузки формы, в пртивном случае не будет центрирования
      });
 }
+
+// для AjaxForm
+function np_AjaxFormSubmit(event) {
+    event.preventDefault();
+    var form_id = $(this).attr('id');
+    if (form_id) { form_id = '#' + form_id };
+    // если не использовать хелпер Html.CheckBox, то установка checkbox обязательна
+    $("input:checkbox").each(function () { // this - это не объект jQuery , а html-element
+        this.value = (this.checked == true);
+    });
+    if ($(form_id + ' input').valid()) {
+        $.ajax({
+            url: $(form_id).attr('action'),
+            data: $(form_id).serialize(),
+            type: 'POST',
+            cache: false,
+            async: false,
+            success: function (data) {
+                //alert(document.location.href);
+                location.reload();
+            },
+            error: function (jqXHR, textStatus, errorThrown) { // панель ошибок формы
+                $(form_id).after("<div class='errorForm'><span></span></div>");
+                $('.errorForm span').html(jqXHR.responseText);
+            }
+        });
+    };
+};
+
+//валидация - сброс ошибок по клику 
+function np_AjaxFormInputClick(event) {  // очистка сообщений об ошибках
+    $("label.error").remove();           // это элемент валидатора
+    $(".errorForm").remove();            // это панель ошибок формы    
+};
