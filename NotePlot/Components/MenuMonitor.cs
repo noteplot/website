530c7d574e997671ffb5def668d5aef4226cd47b
andranick <andranick@bk.ru>
2017-07-16 17:55:06 +0300

    Меню монитора отображается только при регистрации пользователя

---
+++ NotePlot/Components/MenuMonitor.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Http;
+
+namespace NotePlot.Components
+{
+    public class MenuMonitor : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {            
+            return View("MenuMonitorView", HttpContext.User.Identity.IsAuthenticated);
+        }
+
+    }
+}
