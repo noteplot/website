7c7ae90de1d93049b60bfccb02190429b39ea544
Sergey <westwolf@yandex.ru>
2018-06-14 10:44:41 +0300

    Подсветка активного пункта меню.

---
+++ NotePlot/TagHelpers/ActiveNavigationTagHelper.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.AspNetCore.Mvc.Routing;
+using Microsoft.AspNetCore.Mvc.ViewFeatures;
+using Microsoft.AspNetCore.Razor.TagHelpers;
+
+namespace NotePlot.TagHelpers
+{
+
+    [HtmlTargetElement("li", Attributes = _for)]
+    public class ActiveNavigationTagHelper : TagHelper
+    {
+        private readonly IUrlHelperFactory _UrlHelper;
+        private const string _for = "navigation-active-for";
+
+        public string NavigationActiveFor { get; set; }
+
+        [ViewContext]
+        public ViewContext ViewContext { get; set; }
+
+        public ActiveNavigationTagHelper(IUrlHelperFactory urlHelper)
+        {
+            _UrlHelper = urlHelper;
+        }
+
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            var urlHelper = _UrlHelper.GetUrlHelper(ViewContext);
+            var currentPage = urlHelper.Action();
+            if (NavigationActiveFor.EndsWith("*"))
+            {
+                if (currentPage.StartsWith(NavigationActiveFor.TrimEnd('*'), StringComparison.InvariantCultureIgnoreCase))
+                    output.Attributes.Add("class", "active");
+            }
+            else
+            {
+                if (currentPage.Equals(NavigationActiveFor, StringComparison.InvariantCultureIgnoreCase))
+                    output.Attributes.Add("class", "active");
+            }
+        }
+    }
+}
